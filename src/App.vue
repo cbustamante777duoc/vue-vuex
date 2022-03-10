9cb5e0360ce58827f13db4575f2da78c626ecfa2
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-10 13:21:02 -0300

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-<h1>{{$store.state.titleApp}}</h1>
-<h3>{{titleApp}}</h3>
+<button type="button" class="btn btn-primary">Primary</button>
+
 </template>
 
 <script>
 
-
-import {useStore} from 'vuex';
-
 export default {
   name: 'App',
-  setup(){
-    const store = useStore();
 
-    console.log(store.state.titleApp);
-    console.log(store.state.memes);
-
-    //con ...store.state puedo acceder a todas las variables
-    return{
-      ...store.state,
-    };
-
-  }
 }
 </script>
 

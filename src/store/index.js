8b058d370af2b12861973a9a89238d525dc5bf16
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-10 13:12:56 -0300

    uso vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    titleApp:'Memes V3',
+    memes: ['spiderman','batman','robin'],
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-  }
+  },
+  getters:{
+
+  },
 })

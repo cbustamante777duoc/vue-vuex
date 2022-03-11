c967a1a6ba0add98a932e0948e61b894659f353a
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-11 12:32:46 -0300

    uso de mutations y computed

--- src/components/ListMemes.vue
+++ src/components/ListMemes.vue
@@ -1,20 +1,28 @@
 <template>
   <h1>{{title}}</h1>
+  <p>
+      {{memes}}
+  </p>
+
 </template>
 
 <script>
-import {onMounted} from 'vue';
+import {onMounted,computed} from 'vue';
 import {useStore} from 'vuex';
 export default {
     setup(){
         const store = useStore();//inicializacion hook
+        //se hace reactivo para que cuando se actualize conserve los datos
+        const memes = computed(()=>store.state.memes); 
 
+        //apenas se carga el components se carga con los datos
         onMounted(() =>{
             store.dispatch('getMemes');
         });
 
         return{
             title: store.state.titleApp,
+            memes,
         };
     }
 };
--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,21 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     titleApp:'Memes V3',
-    memes: ['spiderman','batman','robin'],
+    memes: [],
   },
   mutations: {
+    setMemes(state,payload) {
+      state.memes = payload; //guardo todos los memes
+
+    },
   },
   actions: {
     async getMemes({commit}){
       try {
         const response = await fetch('https://api.imgflip.com/get_memes');
-        const result = response.json();
-
+        const result =  await response.json();
+        //actualizo mutations con los nuevos datos desde la api
+        commit('setMemes', result.data.memes);
         console.log(result);
       } catch (error) {
         console.log(error);

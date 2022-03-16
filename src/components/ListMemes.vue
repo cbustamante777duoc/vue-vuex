5ec9a0ce1be76bb78c35cfa823beeec4d8102f0d
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-15 21:32:57 -0300

    adding html select of changeTotalShow

--- src/components/ListMemes.vue
+++ src/components/ListMemes.vue
@@ -1,44 +1,62 @@
 <template>
-  <h1>{{title}}</h1>
-  <div class="row">
-      <template v-for="meme in memes" :key="meme.id">
-          <Meme :meme="meme" />
-      </template>
+  <h1>{{ title }}</h1>
+  <div class="total-show">
+    <select @change="changeTotalShow">
+      <option value="">All</option>
+      <option value="5">5</option>
+      <option value="10">10</option>
+      <option value="20">20</option>
+    </select>
   </div>
-
+  <div class="row">
+    <template v-for="meme in memes" :key="meme.id">
+      <Meme :meme="meme" />
+    </template>
+  </div>
 </template>
 
 <script>
-import {onMounted,computed} from 'vue';
-import {useStore} from 'vuex';
-import Meme from './Meme.vue';
+import { onMounted, computed } from "vue";
+import { useStore } from "vuex";
+import Meme from "./Meme.vue";
 export default {
+  components: {
+    Meme,
+  },
 
-    components:{
-        Meme
-    },
+  setup() {
+    const store = useStore(); //inicializacion hook
+    //se hace reactivo para que cuando se actualize conserve los datos
+    const memes = computed(() => store.state.memes);
 
-    setup(){
-        const store = useStore();//inicializacion hook
-        //se hace reactivo para que cuando se actualize conserve los datos
-        const memes = computed(()=>store.state.memes); 
+    console.log(memes);
+    //apenas se carga el components se carga con los datos
+    onMounted(() => {
+      store.dispatch("getMemes");
+    });
 
-        console.log(memes);
-        //apenas se carga el components se carga con los datos
-        onMounted(() =>{
-            store.dispatch('getMemes');
-        });
+    const changeTotalShow = (e) => {
+      console.log("cambio de cantidad");
+      console.log(e.target.value);
+    };
 
-        return{
-            title: store.state.titleApp,
-            memes,
-        };
-    }
+    return {
+      title: store.state.titleApp,
+      memes,
+      changeTotalShow,
+    };
+  },
 };
 </script>
 
 <style scoped>
-    h1 {
-        text-align: center;
-    }
+h1 {
+  text-align: center;
+}
+
+.total-show {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 5px;
+}
 </style>
